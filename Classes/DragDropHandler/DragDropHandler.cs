c3b650abff4dbe6759e43403093915561bc757b9
StephBrochu <stephbrochu@gmail.com>
2026-04-14 13:53:34 -0400

    test v2

--- Classes/DragDropHandler/DragDropHandler.cs
+++ Classes/DragDropHandler/DragDropHandler.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class DragDropHandler : Node
 {
-
+	[Export] public PlayedCard _card;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
