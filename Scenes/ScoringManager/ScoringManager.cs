399bb978b20f7da73e436c9930b77364b146fdf1
StephBrochu <159297706+StephBrochu@users.noreply.github.com>
2026-08-01 07:17:26 -0400

    Added "Show Largest group"

--- Scenes/ScoringManager/ScoringManager.cs
+++ Scenes/ScoringManager/ScoringManager.cs
@@ -1,8 +1,6 @@
 using Godot;
 using System;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 
 public partial class ScoringManager : Control
 {
@@ -11,6 +9,7 @@ public partial class ScoringManager : Control
 	
 	private readonly List<CardCell> _highlightedConditionCells = []; // cells that are highlighted as a scoring condition
 	private readonly List<CardCell> _highlightedScoringCells = []; // cells that are highlighted as scoring
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -23,14 +22,19 @@ public partial class ScoringManager : Control
 	private void CurrentScoring()
 	{
 		
-		var card = (Math.DivRem(_gameData.Season, 2, out _) == 0)
+		var card = _gameData.Season % 2 != 0
 			? _dayCardData.Card[_gameData.DayCard].Front
 			: _dayCardData.Card[_gameData.DayCard].Back;
 		switch (card.CardScoringCondition)
 		{
 			case DayCardData.ScoringConditions.RainInBottomRow:
 				GD.Print("Rain in Bottom Row");
-				ShowBottomRow();
+				ShowBottomRow(3);
+				break;
+			
+			case DayCardData.ScoringConditions.CloudInLargestCloudGroup:
+				GD.Print("Number of Clouds in Largest Cloud Group");
+				ShowLargestGroup(4);
 				break;
 			
 			default:
@@ -38,8 +42,8 @@ public partial class ScoringManager : Control
 				break;
 		}
 	}
-
-	private void ShowBottomRow()
+	
+	private void ShowBottomRow(int icon)
 	{
 		for (int row = (_gameData.Grid.Count-1); row >= 0; row--)
 		{
@@ -47,16 +51,14 @@ public partial class ScoringManager : Control
 			foreach (Cell cell in _gameData.Grid[row].GridRow)
 			{
 				if (cell.Icon == 0) continue;
-				GD.Print(cell.Icon);
 				bottomRow = true;
 				string name = "../" + cell.CardName;
 				DragableCard card = GetNode<DragableCard>(name);
 				var highlightCell = card.Cell[cell.CellIndex];
-				if (cell.Icon == 3)
+				if (cell.Icon == icon)
 				{
 					_highlightedScoringCells.Add(highlightCell);
 					highlightCell.ScoringCellEnable();
-					
 				}
 				else
 				{
@@ -68,6 +70,84 @@ public partial class ScoringManager : Control
 		}
 	}
 
+	private void ShowLargestGroup(int icon)
+	{
+		var allMatchingCellsList = new List<Tuple<int, int>>();
+		var currentGroup = new List<Tuple<int, int>>();
+		var largestGroup = new List<Tuple<int, int>>();
+		
+		//gather the location of all matching cells
+		for (int row = 0; row < (_gameData.Grid.Count); row++)
+		{
+			for (int col = 0; col <_gameData.Grid[row].GridRow.Count; col++)
+			{
+				if (_gameData.Grid[row].GridRow[col].Icon == icon)
+				{
+					Tuple<int, int> coordinates = new Tuple<int, int>(row, col);
+					allMatchingCellsList.Add(coordinates);
+				}
+			}
+		}
+		
+		//Let's go through allMatchingCellsList and see if members are neighbours. 
+		//If they are, add them to currentGroup and keep the largest
+		while (allMatchingCellsList.Count != 0)
+		{
+			//move the top cell coordinate from AllMatchingCellList to the currentGroup list
+			currentGroup.Add(allMatchingCellsList[0]);
+			allMatchingCellsList.RemoveAt(0);
+
+			//itterate through the rest of the list, checking if i
+			int currentGroupCurrentItem = 0;
+			int aMCLCurrentItem = 0; // currrent item from allMatchingCellsListCurrentItem
+			while (currentGroupCurrentItem < currentGroup.Count)
+			{
+				int nextRow = currentGroup[currentGroupCurrentItem].Item1 + 1;
+				int nextCol = currentGroup[currentGroupCurrentItem].Item2 + 1;
+				int prevRow = currentGroup[currentGroupCurrentItem].Item1 - 1;
+				int prevCol = currentGroup[currentGroupCurrentItem].Item2 + 1;
+				// check if the next item on allMatchingCellList is either to the right or below the current item on currentGroup
+				while (aMCLCurrentItem < allMatchingCellsList.Count)
+				{
+					if (allMatchingCellsList[aMCLCurrentItem].Item1 == nextRow && allMatchingCellsList[aMCLCurrentItem].Item2 == currentGroup[currentGroupCurrentItem].Item2   ||
+					    allMatchingCellsList[aMCLCurrentItem].Item1 == currentGroup[currentGroupCurrentItem].Item1 && allMatchingCellsList[aMCLCurrentItem].Item2 == nextCol ||
+						allMatchingCellsList[aMCLCurrentItem].Item1 == prevRow && allMatchingCellsList[aMCLCurrentItem].Item2 == currentGroup[currentGroupCurrentItem].Item2 ||
+					    allMatchingCellsList[aMCLCurrentItem].Item1 == currentGroup[currentGroupCurrentItem].Item1 && allMatchingCellsList[aMCLCurrentItem].Item2 == prevCol )
+					{
+						currentGroup.Add(allMatchingCellsList[aMCLCurrentItem]);
+						allMatchingCellsList.RemoveAt(aMCLCurrentItem);
+					}
+					else
+					{
+						aMCLCurrentItem++;
+					}
+				}
+				currentGroupCurrentItem++;
+			}
+
+			if (currentGroup.Count > largestGroup.Count)
+			{
+				largestGroup.Clear();
+				largestGroup.AddRange(currentGroup);
+				
+			}
+			currentGroup.Clear();
+		}
+		
+		// display the group
+		foreach (var cell in largestGroup)
+		{
+			var row = cell.Item1;
+			var col = cell.Item2;
+			var info = _gameData.Grid[row].GridRow[col];
+			string name = "../" + info.CardName;
+			DragableCard card = GetNode<DragableCard>(name);
+			var highlightCell = card.Cell[info.CellIndex];
+			_highlightedScoringCells.Add(highlightCell);
+			highlightCell.ScoringCellEnable();
+		}
+	}
+	
 	private void HideScoringCell()
 	{
 		foreach (CardCell cell in _highlightedConditionCells)
